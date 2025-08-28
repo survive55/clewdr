9751c04122ffeb7e0abefd68ca882aa6c736cf63
survive55 <survive55@users.noreply.github.com>
2025-08-28 19:49:37 +0800

    Remove clewdr-master directory

--- clewdr-master/RELEASE_NOTES.md
+++
@@ -1,5 +0,0 @@
-# v0.11.13
-
-## Bug Fixes
-
-- Specify log directory for clewdr in Dockerfile
--- clewdr-master/src/services/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod cookie_actor;
-pub mod key_actor;
-#[cfg(feature = "portable")]
-pub mod update;
